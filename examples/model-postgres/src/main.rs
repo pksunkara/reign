1c29eb93478c60676a54f1bedce23513037e6df0
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2021-11-18 22:29:35 +0000

    model: Improve example

--- examples/model-postgres/src/main.rs
+++ examples/model-postgres/src/main.rs
@@ -17,11 +17,6 @@ async fn all_johns() -> Vec<User> {
 
 fn connect() {
     Database::new("postgres://postgres@localhost:5432/reign_examples_model_postgres").connect();
-}
-
-#[tokio::main]
-async fn main() {
-    connect();
     println!("Connected to the database");
 
     Database::get()
@@ -30,6 +25,11 @@ async fn main() {
         .run_pending_migrations(MIGRATIONS)
         .unwrap();
     println!("Ran database migrations");
+}
+
+#[tokio::main]
+async fn main() {
+    connect();
 
     let all_johns = all_johns().await;
     println!("All users named `John`: {:#?}", all_johns);
@@ -38,26 +38,10 @@ async fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use reign::model::tokio_diesel::AsyncSimpleConnection;
-
-    async fn setup() {
-        connect();
-        let conn = Database::get();
-
-        conn.batch_execute_async("DROP TABLE IF EXISTS users")
-            .await
-            .unwrap();
-        conn.batch_execute_async("CREATE TABLE users (id SERIAL, name VARCHAR(255))")
-            .await
-            .unwrap();
-        conn.batch_execute_async("INSERT INTO users (name) VALUES ('John'), ('Sean')")
-            .await
-            .unwrap();
-    }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn test_all() {
-        setup().await;
+        connect();
         let all_johns = all_johns().await;
 
         assert_eq!(all_johns.len(), 1);
